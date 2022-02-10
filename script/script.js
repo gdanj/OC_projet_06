85d6ffe8bc0fba64c818d371d57adcd116b5d4d1
gdanj <gdanj21@gmail.com>
2022-02-10 23:22:43 +0100

    la class est opérationnelle

--- script/script.js
+++ script/script.js
@@ -1,149 +1,152 @@
-const nextUrl = {
-  "url": "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
-}
-
-let tabUrl = []
-let tabResultApi = []
-let currentPosition = 0
-let maxPosition = 19
-
-async function xhrApiMovie(url) {
-  if (tabUrl.includes(url)) {
-    return false
+class Carousel {
+  constructor() {
+    this.nextUrl = {
+      "url": "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
+    }
+    this.buttonRight = document.querySelector("body > main > section:nth-child(1) > div > button.right")
+    this.buttonLeft = document.querySelector("body > main > section:nth-child(1) > div > button.left")
+    this.categoriesDiv = document.querySelector("body > main > section:nth-child(1) .categories-movies")
+    this.tabUrl = []
+    this.tabResultApi = []
+    this.currentPosition = 0
+    this.maxPosition = 19
+    this.moviesDiv1 = document.querySelector("body > main > section:nth-child(1) > div .moviesDiv1")
+    this.moviesDiv2 = document.querySelector("body > main > section:nth-child(1) > div .moviesDiv2")
+    this.moviesDiv3 = document.querySelector("body > main > section:nth-child(1) > div .moviesDiv3")
+    this.moviesDiv4 = document.querySelector("body > main > section:nth-child(1) > div .moviesDiv4")
+    this.moviesDiv5 = document.querySelector("body > main > section:nth-child(1) > div .moviesDiv5")
+    this.categories_movies_nbr_columns = 2
   }
-  tabUrl.push(url)
-  let response = await fetch(url)
-  let apiMovie = await response.json()
-  nextUrl["url"] = apiMovie['next']
-  return apiMovie['results']
-}
+  async xhrApiMovie(url) {
+    if (this.tabUrl.includes(url)) {
+      return false
+    }
+    this.tabUrl.push(url)
+    let response = await fetch(url)
+    let apiMovie = await response.json()
+    this.nextUrl["url"] = apiMovie['next']
+    return apiMovie['results']
+  }
 
-let moviesDiv1 = document.querySelector(".moviesDiv1")
-let moviesDiv2 = document.querySelector(".moviesDiv2")
-let moviesDiv3 = document.querySelector(".moviesDiv3")
-let moviesDiv4 = document.querySelector(".moviesDiv4")
-let moviesDiv5 = document.querySelector(".moviesDiv5")
-let categories_movies_nbr_columns = 2
-
-function refreshVarNbr() {
-  categories_movies_columns = window.getComputedStyle(moviesDiv3).getPropertyValue('grid-template-columns')
-  categories_movies_nbr_columns = categories_movies_columns.split('px').length - 1
-}
-
-async function addDivInHtml(i, tabDiv) {
-  for (let elem of tabDiv) {
-    if (elem.querySelector('img')) {
-      elem.querySelector('img').remove()
+  async addDivInHtml(i, tabDiv) {
+    for (let elem of tabDiv) {
+      if (elem.querySelector('img')) {
+        elem.querySelector('img').remove()
+      }
+    }
+    let j = 0
+    while (j < tabDiv.length) {
+      let tab = []
+      let boocle = i + j > this.maxPosition ? i + j - this.maxPosition - 1 : i + j
+      if (Array.isArray(this.tabResultApi) && this.tabResultApi.length === 0 || this.tabResultApi.length <= boocle && this.tabResultApi.length <= this.maxPosition) {
+        tab = await this.xhrApiMovie(this.nextUrl["url"]).then()
+      }
+      if (tab) {
+        this.tabResultApi.push(...tab)
+      }
+      if (this.tabResultApi[boocle]) {
+        let img = document.createElement('img')
+        img.setAttribute('src', this.tabResultApi[boocle]['image_url'])
+        img.classList.add(`${boocle}`)
+        tabDiv[j].insertAdjacentHTML('beforeend', img.outerHTML)
+      }
+      j++
     }
   }
-  let j = 0
-  console.log(i)
-  while (j < tabDiv.length) {
-    let tab = []
-    let boocle = i + j > maxPosition ? i + j - maxPosition - 1 : i + j
-    if (Array.isArray(tabResultApi) && tabResultApi.length === 0 || tabResultApi.length <= boocle && tabResultApi.length <= maxPosition) {
-      tab = await xhrApiMovie(nextUrl["url"]).then()
+
+  calcPos(nbr) {
+    this.currentPosition = this.currentPosition >= this.maxPosition ? this.currentPosition - this.maxPosition - 1 : this.currentPosition
+    this.currentPosition = 0 > this.currentPosition ? this.maxPosition + 1 + this.currentPosition : this.currentPosition
+    nbr = 0 > nbr ? this.maxPosition + nbr + 1 : nbr
+    if (nbr + this.currentPosition > this.maxPosition) {
+      return (nbr + this.currentPosition - this.maxPosition - 1)
     }
-    if (tab) {
-      tabResultApi.push(...tab)
+    return (nbr + this.currentPosition)
+  }
+
+  replaceImg() {
+    this.moviesDiv1.innerHTML = this.moviesDiv3.innerHTML
+    this.moviesDiv2.innerHTML = this.moviesDiv3.innerHTML
+    this.moviesDiv4.innerHTML = this.moviesDiv3.innerHTML
+    this.moviesDiv5.innerHTML = this.moviesDiv3.innerHTML
+    let tabDiv1 = this.moviesDiv1.querySelectorAll('div')
+    let tabDiv2 = this.moviesDiv2.querySelectorAll('div')
+    let tabDiv3 = this.moviesDiv3.querySelectorAll('div')
+    let tabDiv4 = this.moviesDiv4.querySelectorAll('div')
+    let tabDiv5 = this.moviesDiv5.querySelectorAll('div')
+    this.addDivInHtml(this.calcPos(0), tabDiv3)
+    this.addDivInHtml(this.calcPos(-(2 * tabDiv3.length)), tabDiv1)
+    this.addDivInHtml(this.calcPos(-tabDiv3.length), tabDiv2)
+    this.addDivInHtml(this.calcPos(tabDiv3.length), tabDiv4)
+    this.addDivInHtml(this.calcPos((2 * tabDiv3.length)), tabDiv5)
+  }
+
+  replaceCssGrid(categories_movies_nbr_columns) {
+    this.moviesDiv1.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
+    this.moviesDiv2.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
+    this.moviesDiv3.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
+    this.moviesDiv4.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
+    this.moviesDiv5.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
+  }
+
+  resizeDivMovies() {
+    let i = this.moviesDiv3.querySelectorAll('div').length
+    let div = this.moviesDiv3.querySelector('div')
+    if (div.getBoundingClientRect().width > 250) {
+      while (div.getBoundingClientRect().width > 250) {
+        this.categories_movies_nbr_columns = i + 1
+        this.replaceCssGrid(this.categories_movies_nbr_columns)
+        let newDiv = document.createElement('div')
+        newDiv.classList.add('movie')
+        this.moviesDiv3.insertAdjacentHTML('beforeend', newDiv.outerHTML)
+        i++
+      }
+      this.replaceImg()
     }
-    if (tabResultApi[boocle]) {
-      let img = document.createElement('img')
-      img.setAttribute('src', tabResultApi[boocle]['image_url'])
-      img.classList.add(`${boocle}`)
-      tabDiv[j].insertAdjacentHTML('beforeend', img.outerHTML)
+    if (div.getBoundingClientRect().width < 150) {
+      while (div.getBoundingClientRect().width <= 150) {
+        let tabMoviesDiv = this.moviesDiv3.querySelectorAll('div')
+        this.categories_movies_nbr_columns = tabMoviesDiv.length
+        console.log(tabMoviesDiv[this.categories_movies_nbr_columns - 1])
+        tabMoviesDiv[this.categories_movies_nbr_columns - 1].remove()
+        this.replaceCssGrid(this.categories_movies_nbr_columns - 1)
+      }
+      this.replaceImg()
     }
-    j++
+  }
+
+  remouveClass() {
+    this.replaceImg()
+    this.categoriesDiv.classList.remove("carousel-active-right")
+    this.categoriesDiv.classList.remove("carousel-active-left")
+  }
+
+  waitAnimation(){
+    const mmm = setTimeout(() => test1.remouveClass(), 800)
+  }
+
+  eventCarrossel = () => {
+    test1.buttonRight.addEventListener('click', function () {
+      if (!((test1.categoriesDiv.classList.contains("carousel-active-right") || test1.categoriesDiv.classList.contains("carousel-active-left")))) {
+        test1.categoriesDiv.classList.add("carousel-active-right")
+        test1.currentPosition += test1.moviesDiv3.querySelectorAll('div').length
+        test1.waitAnimation()
+      }
+    })
+    
+    test1.buttonLeft.addEventListener('click', function () {
+      if (!((test1.categoriesDiv.classList.contains("carousel-active-right") || test1.categoriesDiv.classList.contains("carousel-active-left")))) {
+        test1.categoriesDiv.classList.add("carousel-active-left")
+        test1.currentPosition -= test1.moviesDiv3.querySelectorAll('div').length
+        test1.waitAnimation()
+      }
+    })
+    test1.resizeDivMovies()
+    window.addEventListener('resize', function () {
+      test1.resizeDivMovies()
+    })
   }
 }
 
-function calcPos(nbr) {
-  currentPosition = currentPosition >= maxPosition ? currentPosition - maxPosition - 1 : currentPosition
-  currentPosition = 0 > currentPosition ? maxPosition + 1 + currentPosition : currentPosition
-  nbr = 0 > nbr ? maxPosition + nbr + 1 : nbr
-  if (nbr + currentPosition > maxPosition) {
-    return (nbr + currentPosition - maxPosition - 1)
-  }
-  return (nbr + currentPosition)
-}
-
-function replaceImg() {
-  moviesDiv1.innerHTML = moviesDiv3.innerHTML
-  moviesDiv2.innerHTML = moviesDiv3.innerHTML
-  moviesDiv4.innerHTML = moviesDiv3.innerHTML
-  moviesDiv5.innerHTML = moviesDiv3.innerHTML
-  let tabDiv1 = moviesDiv1.querySelectorAll('div')
-  let tabDiv2 = moviesDiv2.querySelectorAll('div')
-  let tabDiv3 = moviesDiv3.querySelectorAll('div')
-  let tabDiv4 = moviesDiv4.querySelectorAll('div')
-  let tabDiv5 = moviesDiv5.querySelectorAll('div')
-  addDivInHtml(calcPos(0), tabDiv3)
-  addDivInHtml(calcPos(-(2 * tabDiv3.length)), tabDiv1)
-  addDivInHtml(calcPos(-tabDiv3.length), tabDiv2)
-  addDivInHtml(calcPos(tabDiv3.length), tabDiv4)
-  addDivInHtml(calcPos((2 * tabDiv3.length)), tabDiv5)
-}
-
-function replaceCssGrid(categories_movies_nbr_columns) {
-  moviesDiv1.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
-  moviesDiv2.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
-  moviesDiv3.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
-  moviesDiv4.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
-  moviesDiv5.style.gridTemplateColumns = `repeat(${categories_movies_nbr_columns}, 1fr)`
-}
-
-function resizeDivMovies() {
-  let i = moviesDiv3.querySelectorAll('div').length
-  let div = moviesDiv3.querySelector('div')
-  if (div.getBoundingClientRect().width > 250) {
-    while (div.getBoundingClientRect().width > 250) {
-      categories_movies_nbr_columns = i + 1
-      replaceCssGrid(categories_movies_nbr_columns)
-      let newDiv = document.createElement('div')
-      newDiv.classList.add('movie')
-      moviesDiv3.insertAdjacentHTML('beforeend', newDiv.outerHTML)
-      i++
-    }
-    replaceImg()
-  }
-  if (div.getBoundingClientRect().width < 150) {
-    while (div.getBoundingClientRect().width < 150) {
-      let tabMoviesDiv = moviesDiv3.querySelectorAll('div')
-      categories_movies_nbr_columns = tabMoviesDiv.length
-      tabMoviesDiv[categories_movies_nbr_columns - 1].remove()
-      replaceCssGrid(categories_movies_nbr_columns - 1)
-    }
-    replaceImg()
-  }
-}
-
-buttonRight = document.querySelector("body > main > section:nth-child(1) > div > button.right")
-buttonLeft = document.querySelector("body > main > section:nth-child(1) > div > button.left")
-let categoriesDiv = document.querySelector("#best-rated")
-
-function remouveClass() {
-  replaceImg()
-  categoriesDiv.classList.remove("carousel-active-right")
-  categoriesDiv.classList.remove("carousel-active-left")
-}
-buttonRight.addEventListener('click', function () {
-  if (!((categoriesDiv.classList.contains("carousel-active-right") || categoriesDiv.classList.contains("carousel-active-left")))) {
-    console.log("eeeeeeee")
-    categoriesDiv.classList.add("carousel-active-right")
-    currentPosition += moviesDiv3.querySelectorAll('div').length
-    const myTimeout2 = setTimeout(remouveClass, 800);
-  }
-})
-
-buttonLeft.addEventListener('click', function () {
-  if (!(categoriesDiv.classList.contains("carousel-active-right") || categoriesDiv.classList.contains("carousel-active-left"))) {
-    categoriesDiv.classList.add("carousel-active-left")
-    currentPosition -= moviesDiv3.querySelectorAll('div').length
-    const myTimeout2 = setTimeout(remouveClass, 800);
-  }
-})
-
-resizeDivMovies()
-window.addEventListener('resize', function () {
-  resizeDivMovies()
-})
+test1 = new Carousel()
+test1.eventCarrossel()
