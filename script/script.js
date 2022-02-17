af4eb908d46d73be1478febafaea0231b8d3360b
gdanj <gdanj21@gmail.com>
2022-02-17 15:09:28 +0100

    maj du button left du slider

--- script/script.js
+++ script/script.js
@@ -17,25 +17,41 @@ class Carousel {
     this.moviesDiv5 = document.querySelector(`body > main > section:nth-child(${nbr}) > div .moviesDiv5`)
     this.categories_movies_nbr_columns = 2
   }
+
+  async jsonData(response) {
+    let apiMovie = await response.json()
+    this.nextUrl["url"] = apiMovie['next']
+    for (let elem of apiMovie['results']){
+      if (elem['id'] !== 1508669){
+        this.tabResultApi.push(elem)
+      }
+    }
+    return true
+  }
+
   async xhrApiMovie(url) {
     if (this.tabUrl.includes(url)) {
       return false
     }
     this.tabUrl.push(url)
-    let response = await fetch(url).then()
-    let apiMovie = await response.json()
-    this.nextUrl["url"] = apiMovie['next']
-    return apiMovie['results']
+    await fetch(url).then(response => {
+      return this.jsonData(response)
+    })
   }
 
   async addText(elem, modal_container) {
     let tab = ["title", "year", "duration", "imdb_score", "votes", "original_title", "countries", "genres", "actors", "description", "directors", "writers", "company", "languages"]
     await fetch(`http://localhost:8000/api/v1/titles/${elem.dataset["movie"]}`).then(dataJson => {
       dataJson.json().then(data => {
-        console.log(data)
         for (let classs of tab) {
           let element = modal_container.querySelector(`.modal .${classs}`)
-          element.innerText = data[classs]
+          if (classs === "duration") {
+            element.innerText = `${parseInt(data[classs] / 60)}h${data[classs] % 60 > 9 ? data[classs] % 60: `0${data[classs] % 60}`}`
+          }else if(Array.isArray(data[classs])) {
+            element.innerText = data[classs].toString().replaceAll(',', ', ')
+          }else {
+            element.innerText = data[classs]
+          }
         }
         modal_container.querySelector(`img`).setAttribute('src', data["image_url"])
       })
@@ -50,13 +66,9 @@ class Carousel {
     }
     let j = 0
     while (j < tabDiv.length) {
-      let tab = []
       let boocle = i + j > this.maxPosition ? i + j - this.maxPosition - 1 : i + j
       if (Array.isArray(this.tabResultApi) && this.tabResultApi.length === 0 || this.tabResultApi.length <= boocle && this.tabResultApi.length <= this.maxPosition) {
-        tab = await this.xhrApiMovie(this.nextUrl["url"]).then()
-      }
-      if (tab) {
-        this.tabResultApi.push(...tab)
+        await this.xhrApiMovie(this.nextUrl["url"])
       }
       if (this.tabResultApi[boocle]) {
         let img = document.createElement('img')
@@ -84,7 +96,7 @@ class Carousel {
     return (nbr + this.currentPosition)
   }
 
-  replaceImg() {
+  async replaceImg() {
     this.moviesDiv1.innerHTML = this.moviesDiv3.innerHTML
     this.moviesDiv2.innerHTML = this.moviesDiv3.innerHTML
     this.moviesDiv4.innerHTML = this.moviesDiv3.innerHTML
@@ -94,11 +106,11 @@ class Carousel {
     let tabDiv3 = this.moviesDiv3.querySelectorAll('div')
     let tabDiv4 = this.moviesDiv4.querySelectorAll('div')
     let tabDiv5 = this.moviesDiv5.querySelectorAll('div')
-    this.addDivInHtml(this.calcPos(0), tabDiv3)
-    this.addDivInHtml(this.calcPos(-(2 * tabDiv3.length)), tabDiv1)
-    this.addDivInHtml(this.calcPos(-tabDiv3.length), tabDiv2)
+    await this.addDivInHtml(this.calcPos(0), tabDiv3)
     this.addDivInHtml(this.calcPos(tabDiv3.length), tabDiv4)
+    this.addDivInHtml(this.calcPos(-tabDiv3.length), tabDiv2)
     this.addDivInHtml(this.calcPos((2 * tabDiv3.length)), tabDiv5)
+    this.addDivInHtml(this.calcPos(-(2 * tabDiv3.length)), tabDiv1)
   }
 
   replaceCssGrid(categories_movies_nbr_columns) {
@@ -127,7 +139,6 @@ class Carousel {
       while (div.getBoundingClientRect().width <= 150) {
         let tabMoviesDiv = this.moviesDiv3.querySelectorAll('div')
         this.categories_movies_nbr_columns = tabMoviesDiv.length
-        console.log(tabMoviesDiv[this.categories_movies_nbr_columns - 1])
         tabMoviesDiv[this.categories_movies_nbr_columns - 1].remove()
         this.replaceCssGrid(this.categories_movies_nbr_columns - 1)
       }
@@ -141,6 +152,11 @@ class Carousel {
     this.categoriesDiv.classList.remove("carousel-active-left")
   }
 
+  buttonLeftVisible(){
+    this.categoriesDiv.parentElement.querySelector(".left").style.visibility = "visible"
+    this.moviesDiv2.querySelector("div:last-child").style.visibility = "visible"
+  }
+
   waitAnimation() {
     const mmm = setTimeout(() => this.remouveClass(), 800)
   }
@@ -151,26 +167,53 @@ class Carousel {
         this.categoriesDiv.classList.add("carousel-active-right")
         this.currentPosition += this.moviesDiv3.querySelectorAll('div').length
         this.waitAnimation()
+        this.buttonLeftVisible()
       }
     })
-
+    
     this.buttonLeft.addEventListener('click', () => {
       if (!((this.categoriesDiv.classList.contains("carousel-active-right") || this.categoriesDiv.classList.contains("carousel-active-left")))) {
         this.categoriesDiv.classList.add("carousel-active-left")
         this.currentPosition -= this.moviesDiv3.querySelectorAll('div').length
         this.waitAnimation()
+        this.buttonLeftVisible()
       }
     })
     this.resizeDivMovies()
     window.addEventListener('resize', () => {
       this.resizeDivMovies()
+      this.buttonLeftVisible()
     })
+    this.moviesDiv2.querySelector("div:last-child").style.visibility = "hidden"
   }
 }
 
+let tabEventHeader = document.querySelectorAll(".modal-info")
+tabEventHeader.forEach(elem => elem.addEventListener("click", () => {
+    addText(elem, modal_container)
+    modal_container.classList.toggle("active")
+}))
+
+async function addText(elem, modal_container) {
+  let tab = ["title", "year", "duration", "imdb_score", "votes", "original_title", "countries", "genres", "actors", "description", "directors", "writers", "company", "languages"]
+  await fetch(`http://localhost:8000/api/v1/titles/${elem.dataset["movie"]}`).then(dataJson => {
+    dataJson.json().then(data => {
+      for (let classs of tab) {
+        let element = modal_container.querySelector(`.modal .${classs}`)
+        if (classs === "duration") {
+          element.innerText = `${parseInt(data[classs] / 60)}h${data[classs] % 60 > 9 ? data[classs] % 60: `0${data[classs] % 60}`}`
+        } else {
+          element.innerText = data[classs]
+        }
+      }
+      modal_container.querySelector(`img`).setAttribute('src', data["image_url"])
+    })
+  })
+}
+
 let modal_container = document.querySelector(".modal-container")
-let modal_trigger = document.querySelectorAll(".modal-trigger")
-modal_trigger.forEach(elem => elem.addEventListener("click", () => {
+let modal_trigger = document.querySelectorAll(".modal-trigger:not(body > header > div.movie > img)")
+modal_trigger.forEach(elem => elem.addEventListener("click", (e) => {
   modal_container.classList.toggle("active")
 }))
 
